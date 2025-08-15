2b47f769133256e2bd451a7ce225dfc00379ffc1
supriya varhade <balajivarhade@gmail.com>
2025-08-15 13:58:56 +0530

    Add files via upload

---
+++ Assignment5/question1.py
@@ -0,0 +1,11 @@
+correct_userid = "admin"
+correct_password = "1234"
+
+for i in range(3):
+    userid = input("Enter username:")
+    password = input("Enter password:")
+    if userid == correct_userid and password == correct_password:
+        print("Login successfully")
+        break
+    else:
+     print("Invalid credential! Please try again")
